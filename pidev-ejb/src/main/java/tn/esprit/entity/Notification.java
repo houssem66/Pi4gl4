2d29d13eee95bcca3166c5a6fb74bc0834e9b91f
inesISSAOUI <47125388+inesISSAOUI@users.noreply.github.com>
2019-10-31 07:05:46 +0100

    Add files via upload
    TimesheetEntities

---
+++ pidev-ejb/src/main/java/tn/esprit/entity/Notification.java
@@ -0,0 +1,53 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Notification implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	int id;
+	String description;
+	@ManyToOne
+	Employe employe;
+    
+	public Notification(int id, String description, Employe employe) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.employe = employe;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Employe getEmploye() {
+		return employe;
+	}
+	public void setEmploye(Employe employe) {
+		this.employe = employe;
+	}
+	
+	
+
+}
---
+++ pidev-ejb/src/main/java/tn/esprit/entity/Timesheet.java
@@ -0,0 +1,65 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Timesheet implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	int id;
+	String designation;
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="timesheet")
+	Set<Tache> taches;
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="timesheet")
+	Set<TravailParJour> travailParJour;
+	
+	
+	
+	public Timesheet(int id, String designation, Set<Tache> taches, Set<TravailParJour> travailParJour) {
+		super();
+		this.id = id;
+		this.designation = designation;
+		this.taches = taches;
+		this.travailParJour = travailParJour;
+	}
+	
+	public Timesheet() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public Set<TravailParJour> getTravailParJour() {
+		return travailParJour;
+	}
+	public void setTravailParJour(Set<TravailParJour> travailParJour) {
+		this.travailParJour = travailParJour;
+	}
+	
+	
+	
+
+}
