bacc34f7b68972919d0472f22f19f764b5538738
inesISSAOUI <47125388+inesISSAOUI@users.noreply.github.com>
2019-10-31 07:27:34 +0100

    Add files via upload

---
+++ pidev-ejb/src/main/java/tn/esprit/service/interfaces/IBonPointServiceLocal.java
@@ -0,0 +1,13 @@
+package iservices;
+
+import javax.ejb.Local;
+
+import entities.BonPoint;
+
+@Local
+public interface IBonPointServiceLocal {
+public void add(BonPoint bp);
+public void update(BonPoint bp);
+public void delete(BonPoint bp);
+public BonPoint findById(int id);
+}
